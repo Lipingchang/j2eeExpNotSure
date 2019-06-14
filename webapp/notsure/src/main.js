a0a81f4e86accbe9d889a0b9a34a5723b1604cdd
Unknown <unknown@example.com>
2019-06-15 01:47:27 +0800

    添加axios的拦截器，拦截权限不够的情况

--- webapp/notsure/src/main.js
+++ webapp/notsure/src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import {Message} from 'element-ui'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
@@ -17,6 +18,32 @@ var instance = axios.create({
   timeout:1000,
   withCredentials: true 
 });
+
+//请求返回拦截，把数据返回到页面之前做些什么...
+instance.interceptors.response.use((response) => {
+  // console.log('interceptors',response)
+  return response;
+//   //特殊错误处理，状态为10时为登录超时
+//   if (response.data.code === 10) {
+//     // iView.Message.error("登录已超时，请重新登录");
+//     // router.push("/login")
+//   //其余错误状态处理    
+//   } else if (response.data.code != 0) {
+//     // iView.Message.error(response.data.msg)
+//   //请求成功
+//   } else if(response.data.code === 0){
+//     //将我们请求到的信息返回页面中请求的逻辑
+//     return response;
+//   }
+//  //......
+
+}, function (error) {
+  if ( error.response.status == 401 ){
+    Message.error('权限不足，不能访问');
+  }
+  return Promise.reject(error);
+});
+
 
 Vue.prototype.$axios= instance
 Vue.use(Vuex)
