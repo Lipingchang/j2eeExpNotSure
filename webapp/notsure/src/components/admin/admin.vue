c972b234d20783c760b70460566b0661fa3b4dc3
Unknown <unknown@example.com>
2019-06-16 15:30:03 +0800

    权限判断后决定显示的栏目
    还添加了默认路由

--- webapp/notsure/src/components/admin/admin.vue
+++ webapp/notsure/src/components/admin/admin.vue
@@ -2,23 +2,23 @@
 <el-container>
   <el-aside width="200px">
 
-    <el-menu>
+    <el-menu :default-active="subMenus[defaultSubMenue]" >
     <div class="userinfo">
       <i class="el-icon-apple"></i>
       <span>{{this.$store.state.userinfo.username}}<i>{{this.$store.state.userinfo.rolename}}</i></span>
     </div>
 
-      <el-submenu index="1">
+      <el-submenu index="1" v-if="hasAuthority('channelAdmin')">
         <template slot="title"><i class="el-icon-takeaway-box"></i>栏目管理</template>          
         <router-link to="/channels">
           <el-menu-item index="1-1">栏目管理</el-menu-item>
         </router-link>
       </el-submenu>
-      <el-submenu index="2">
+      <el-submenu index="2" v-if="hasAuthority('articleAdmin')">
         <template slot="title"><i class="el-icon-files"></i>文章管理</template>
         <router-link to="/articles"><el-menu-item index="2-1">文章管理</el-menu-item></router-link>
       </el-submenu>
-      <el-submenu index="3">
+      <el-submenu index="3" v-if="hasAuthority('personAdmin')">
         <template slot="title"><i class="el-icon-user"></i>人员管理</template>
         <router-link to="/persons"><el-menu-item index="3-1">人员管理</el-menu-item></router-link>
       </el-submenu>
@@ -39,6 +39,39 @@
 export default {
     data() {
       return {
+        auths: {
+          'channelAdmin': [
+            'ROLE_管理栏目'
+          ],
+          'articleAdmin': [
+            'ROLE_浏览文章',
+            'ROLE_删除文章',
+            'ROLE_编辑文章',
+            'ROLE_添加文章'
+          ],
+          'personAdmin': [
+            'ROLE_人员管理'
+          ]
+        },
+        subMenus: {
+          'channels': '1-1',
+          'articles': '2-1',
+          'persons': '3-1'
+        },
+        defaultSubMenue: 'channels'
+      }
+    },
+    mounted: function (){
+      this.$router.replace( '/' + this.defaultSubMenue )
+    },
+    methods: {
+      hasAuthority: function ( menueName ){
+        let userAuth = this.$store.state.userinfo.authorities
+        for( let i in userAuth ){
+          if ( this.auths[menueName].includes(userAuth[i]['authority']) )
+            return true
+        }
+        return false
       }
     }
 }
